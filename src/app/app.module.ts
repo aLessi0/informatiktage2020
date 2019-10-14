5744d51bfad874ffd3f3242cb2a300180d72554e
Alessio <alessio.romagnolo@gmail.com>
2019-10-14 10:30:16 +0200

    App im 9/21 Format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {QuizRoomComponent} from './components/quiz-room/quiz-room.component';
+import {MapComponent} from './components/map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizRoomComponent,
+    MapComponent,
+    QuizRoomComponent
   ],
   imports: [
     BrowserModule,
