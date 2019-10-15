da161edb6a8e0a8c3b0aaa60ca7a04860470da10
nimapa <nparpan@gmail.com>
2019-10-15 13:38:53 +0200

    rooooooom

--- src/app/components/room/room.component.ts
+++ src/app/components/room/room.component.ts
@@ -1,23 +1,31 @@
-import {Component, EventEmitter, Inject, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Inject, Input, OnInit, Output} from '@angular/core';
 import {RoomModel} from '../../model/game/room.model';
-import {QuestionModel} from '../../model/game/question.model';
+import {FeedbackComponent} from '../modal/feedback/feedback.component';
 import {DataService} from '../../service/data.service';
 import {ModalService} from '../../service/modal.service';
+import {QuestionModel} from '../../model/game/question.model';
 import {QuizfrageComponent} from '../base/quizfrage/quizfrage.component';
-import {FeedbackComponent} from "../modal/feedback/feedback.component";
 
 @Component({
   selector: 'app-room',
   templateUrl: './room.component.html',
   styleUrls: ['./room.component.scss']
 })
-export class RoomComponent {
+export class RoomComponent implements OnInit {
   @Input() public room: RoomModel;
   @Output() private onClose: EventEmitter<void> = new EventEmitter();
 
+  public optionalQuestions: number[];
+
   constructor(@Inject(DataService) private readonly dataService: DataService,
               @Inject(ModalService) private readonly modalService: ModalService) {
+  }
 
+  public ngOnInit(): void {
+    this.optionalQuestions = [];
+    for (let i = 1; i < this.room.questions.length; i++) {
+      this.optionalQuestions.push(i);
+    }
   }
 
   public closeRoom(): void {
