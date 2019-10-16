78620965d225180767fcead79122c5f3f70bed21
nimapa <nparpan@gmail.com>
2019-10-16 09:11:26 +0200

    rooms initial setup

--- src/app/components/room/1-welt-der-informatik/welt-der-informatik.component.ts
+++ src/app/components/room/1-welt-der-informatik/welt-der-informatik.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {DataService} from '../../../service/data.service';
+import {ModalService} from '../../../service/modal.service';
+import {AbstractRoom} from '../abstract-room';
 
 @Component({
   selector: 'app-welt-der-informatik',
   templateUrl: './welt-der-informatik.component.html',
   styleUrls: ['./welt-der-informatik.component.scss']
 })
-export class WeltDerInformatikComponent implements OnInit {
+export class WeltDerInformatikComponent extends AbstractRoom {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(@Inject(DataService) protected readonly dataService: DataService,
+              @Inject(ModalService) protected readonly modalService: ModalService) {
+    super(dataService, modalService);
   }
 
 }
--- src/app/components/room/2-requirement-design/requirement-design.component.ts
+++ src/app/components/room/2-requirement-design/requirement-design.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject} from '@angular/core';
+import {AbstractRoom} from '../abstract-room';
+import {DataService} from '../../../service/data.service';
+import {ModalService} from '../../../service/modal.service';
 
 @Component({
   selector: 'app-requirement-design',
   templateUrl: './requirement-design.component.html',
   styleUrls: ['./requirement-design.component.scss']
 })
-export class RequirementDesignComponent implements OnInit {
+export class RequirementDesignComponent extends AbstractRoom {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(@Inject(DataService) protected readonly dataService: DataService,
+              @Inject(ModalService) protected readonly modalService: ModalService) {
+    super(dataService, modalService);
   }
 
 }
