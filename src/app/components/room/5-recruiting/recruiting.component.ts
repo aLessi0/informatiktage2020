e02996828c5117360cba77a48637a0ae5bf0b2f1
Roman Mathys <info@roman-mathys.com>
2019-10-17 11:22:48 +0200

    design room 5

--- src/app/components/room/5-recruiting/recruiting.component.ts
+++ src/app/components/room/5-recruiting/recruiting.component.ts
@@ -2,7 +2,6 @@ import {Component, Inject} from '@angular/core';
 import {AbstractRoom} from '../abstract-room';
 import {DataService} from '../../../service/data.service';
 import {ModalService} from '../../../service/modal.service';
-import {FeedbackInformatiktageComponent} from '../../base/feedback/feedback-informatiktage.component';
 
 @Component({
   selector: 'app-recruiting',
@@ -14,10 +13,6 @@ export class RecruitingComponent extends AbstractRoom {
   constructor(@Inject(DataService) protected readonly dataService: DataService,
               @Inject(ModalService) protected readonly modalService: ModalService) {
     super(dataService, modalService);
-  }
-
-  public openITDFeedback(): void {
-    this.modalService.openDialog(FeedbackInformatiktageComponent, false).subscribe(() => 1);
   }
 
 }
