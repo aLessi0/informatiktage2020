2e6bffc16355989eba4a875845cb4d9536088ced
Benjamin Pereto <benjamin@sandchaschte.ch>
2019-10-17 09:59:11 +0200

    FEATURE: gamification for room 3

--- src/app/components/room/3-entwicklung/entwicklung.component.ts
+++ src/app/components/room/3-entwicklung/entwicklung.component.ts
@@ -1,4 +1,4 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, Input} from '@angular/core';
 import {AbstractRoom} from '../abstract-room';
 import {DataService} from '../../../service/data.service';
 import {ModalService} from '../../../service/modal.service';
@@ -13,6 +13,49 @@ export class EntwicklungComponent extends AbstractRoom {
   constructor(@Inject(DataService) protected readonly dataService: DataService,
               @Inject(ModalService) protected readonly modalService: ModalService) {
     super(dataService, modalService);
+  }
+
+  @Input('star-id') starID;
+
+  private planetRewardCollected: boolean = false;
+  private planetState: number = 0;
+
+  private starRewardCollected: boolean = false;
+  private starsHidden = [];
+
+  planetClick() {
+    console.log(this.planetState);
+    if ( this.planetRewardCollected ) {
+      return;
+    }
+
+    this.planetState++;
+
+    if (this.planetState >= 3) {
+      console.log("QUESTION!");
+      this.openQuestion(this.room.optionalQuestions[0]);
+      this.planetRewardCollected = true;
+    }
+  }
+
+  starClick(id) {
+    console.log(id);
+    if ( !this.isStarHidden(id) ) {
+      this.starsHidden.push(id);
+    }
+
+    //check reward
+    if ( this.starsHidden.length == 4 &&  !this.starRewardCollected ) {
+      console.log('COIN!');
+    }
+    console.log(this.starsHidden)
+  }
+
+  isStarHidden(id) {
+    if ( this.starsHidden.indexOf(id) !== -1) {
+      return true;
+    }
+    return false;
   }
 
 }
