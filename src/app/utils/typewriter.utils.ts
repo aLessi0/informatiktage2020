595cac9816b23e5d935d1e95f8d62320b9271000
rbnfrhnr <frehner.robin@gmail.com>
2019-12-02 11:21:17 +0100

    typewriter implementiert

---
+++ src/app/utils/typewriter.utils.ts
@@ -0,0 +1,24 @@
+export class TypewriterUtils {
+
+  public static typewrite(element: HTMLElement, text: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+
+      if (element && text) {
+
+        const speed = 50;
+        let letterIndex = 0;
+
+        const intervalId = setInterval(() => {
+          element.innerHTML = element.innerHTML += text[letterIndex];
+          letterIndex++;
+          if (letterIndex === text.length) {
+            clearInterval(intervalId);
+            resolve();
+          }
+        }, speed);
+      } else {
+        reject();
+      }
+    });
+  }
+}
